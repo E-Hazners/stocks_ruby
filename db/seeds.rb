7726a28dd3719731863057e3fd47863e0859fa70
Edvards <bombadourada@gmail.com>
2022-11-06 20:50:42 +0200

    commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,7 @@ CSV.foreach(Rails.root.join('./lib/csvs/SP500.csv'), headers: true) do |row|
   date = Date.strptime(row[0], '%m/%d/%y')
 
   SStock.create({
-    date: date.strftime('%y/%m/%d')
+    date: date.strftime('%y/%m/%d'),
     open: row[1],
     high: row[2],
     low: row[3],
